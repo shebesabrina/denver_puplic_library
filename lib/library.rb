1b2a4d4cb8d409f60735b97065fe2fa9b26ad2b5
Sabrina Robinson <sabrinarob@gmail.com>
2018-04-11 10:55:02 -0600

    Finishing

--- lib/library.rb
+++ lib/library.rb
@@ -7,4 +7,16 @@ class Library
   def add_to_collection(title)
     @books << title
   end
+
+  def include?(title)
+    @books.flatten.any? do |book|
+      book.title == title
+    end
+  end
+
+  def card_catalogue
+    @books.max_by do |book|
+      book.first.author_last_name.length
+    end
+  end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -23,12 +23,44 @@ class LibraryTest < Minitest::Test
     jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
     villette  = charlotte_bronte.add_book("Villette", "1853")
     mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
-    result_1 = dpl.add_to_collection(jane_eyre)
-    result_2 = dpl.add_to_collection(villette)
-    result_3 = dpl.add_to_collection(mockingbird)
 
-    assert_equal "Jane Eyre", result_1.flatten.first.title
-    assert_equal "Villette", result_2.flatten[1].title
-    assert_equal "July 11, 1960", result_3.flatten.last.publication_date
+    dpl.add_to_collection(jane_eyre)
+    dpl.add_to_collection(villette)
+    dpl.add_to_collection(mockingbird)
+
+    assert_equal "Jane Eyre", jane_eyre.first.title
+    assert_equal "Villette", villette[1].title
+    assert_equal "July 11, 1960", mockingbird.last.publication_date
+  end
+
+  def test_the_collection_includes_the_book_attributes
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_to_collection(jane_eyre)
+    dpl.add_to_collection(villette)
+    dpl.add_to_collection(mockingbird)
+
+    assert_equal true, dpl.include?("To Kill a Mockingbird")
+    assert_equal true, dpl.include?("Villette")
+    assert_equal true, dpl.include?("Jane Eyre")
+    assert_equal false, dpl.include?("Harry Potter")
+  end
+
+  def test_collection_can_be_sorted
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_to_collection(jane_eyre)
+    dpl.add_to_collection(villette)
+    dpl.add_to_collection(mockingbird)
+
+    assert_equal "Bronte", dpl.card_catalogue.first.author_last_name
   end
 end
