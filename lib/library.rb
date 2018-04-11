07a2e850aefb3061a79deb752ef73eb9a2098dd3
Sabrina Robinson <sabrinarob@gmail.com>
2018-04-11 10:19:45 -0600

    Adding tests to library class.

--- lib/library.rb
+++ lib/library.rb
@@ -1,2 +1,10 @@
 class Library
+  attr_reader :books
+  def initialize
+    @books = []
+  end
+
+  def add_to_collection(title)
+    @books << title
+  end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -1,5 +1,7 @@
 require './test/test_helper'
 require './lib/library'
+require './lib/author'
+require './lib/book'
 
 class LibraryTest < Minitest::Test
   attr_reader :dpl
@@ -9,5 +11,24 @@ class LibraryTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Library, dpl
+  end
+
+  def test_books_starts_empty
+    assert_equal [], dpl.books
+  end
+
+  def test_add_collection_of_book
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+    result_1 = dpl.add_to_collection(jane_eyre)
+    result_2 = dpl.add_to_collection(villette)
+    result_3 = dpl.add_to_collection(mockingbird)
+
+    assert_equal "Jane Eyre", result_1.flatten.first.title
+    assert_equal "Villette", result_2.flatten[1].title
+    assert_equal "July 11, 1960", result_3.flatten.last.publication_date
   end
 end
