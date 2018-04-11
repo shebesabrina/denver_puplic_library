0a3d19ca9fa8e3a332d6f641162b9056d8209dc8
Sabrina Robinson <sabrinarob@gmail.com>
2018-04-11 09:09:09 -0600

    Initial commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,15 @@
 class Book
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
+
+  def initialize(argument)
+    @author_first_name = argument[:author_first_name]
+    @author_last_name = argument[:author_last_name]
+    @title = argument[:title]
+    @publication_date = argument[:publication_date]
+  end
 
 
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,18 @@
 require './test/test_helper'
+require './lib/book'
 
 class BookTest < Minitest::Test
+  attr_reader :book
+  def setup
+    @book = Book.new(
+      {author_first_name: "Harper",
+        author_last_name: "Lee",
+        title: "To Kill a Mockingbird",
+        publication_date: "July 11, 1960"
+        })
+  end
 
-
+  def test_it_exists
+    assert_instance_of Book, book
+  end
 end
